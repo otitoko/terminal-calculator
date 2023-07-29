438b82530c130f0d840637b99b15f5c45c8f8c11
otitoko <tbkopera@proton.me>
2023-07-29 16:13:10 +0100

    seems to be working. simple little project

--- calculator.c
+++ calculator.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 
+void addition(float* number_one, float* number_two, float* result);
+void subtraction(float* number_one, float* number_two, float* result);
+void multiplication(float* number_one, float* number_two, float* result);
+void division(float* number_one, float* number_two, float* result);
+int decimalPointCheck(float* result_ptr);
 
 int main(){
 	float first_number;
@@ -19,14 +26,76 @@ int main(){
 	scanf("%f", &first_number);
 	printf("\nType in the second number:");
 	scanf("%f", &second_number);
+	
 
 
-	if(operator == 1){
-		result = first_number + second_number;
+	switch (operator){
+		case 1:
+			addition(&first_number,&second_number,&result);
+			break;
+		case 2:
+			subtraction(&first_number, &second_number,&result);
+			break;
+		case 3:
+			multiplication(&first_number, &second_number, &result);
+			break;
+		case 4:
+			division(&first_number,&second_number,&result);
+			break;
 	}
 
-	printf("%f",result);
+	printf("%.2f\n",result);
+	if(decimalPointCheck(&result)){
+		char check;
+
+		printf("The result contained numbers past the second decimal point. Would you like to view the full number? Y/n \n");
+		scanf(" %c", &check);
+
+		switch(check){
+			case 'y':
+				printf("%f",result);
+				break;
+			case 'n':
+				break;
+		}
+		
+	}
+
+	return 0;
 
 
 }
+
+
+void addition(float* number_one,float* number_two,float* result){
+	*result = *number_one + *number_two;
+	
+
+}
+
+void subtraction(float* number_one, float* number_two, float* result){
+	*result= *number_one - *number_two;
+}
+
+void multiplication(float* number_one,float* number_two, float* result){
+	*result=*number_one * *number_two;
+}
+
+void division(float* number_one, float* number_two, float* result){
+	*result= *number_one / *number_two;
+}
+
+int decimalPointCheck(float* result_ptr){
+	float quotient = *result_ptr - (int)*result_ptr;
+
+	if (quotient >= .01){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+	
+
 
